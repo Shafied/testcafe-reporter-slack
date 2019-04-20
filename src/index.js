9c2da07ad916e593dfc9e732e49b243014250ff9
Marcin Łesek <daav95@gmail.com>
2019-04-21 00:20:10 +0200

    Code consistency reformat

--- src/index.js
+++ src/index.js
@@ -4,74 +4,74 @@ import loggingLevels from './const/LoggingLevels';
 import emojis from './utils/emojis';
 import { bold, italics } from './utils/textFormatters';
 
-const { loggingLevel } = config;
+const {loggingLevel} = config;
 
 export default function () {
-    return {
+  return {
 
-        noColors: true,
+    noColors: true,
 
-        reportTaskStart (startTime, userAgents, testCount) {
-            this.slack = new SlackMessage();
-            this.startTime = startTime;
-            this.testCount = testCount;
+    reportTaskStart(startTime, userAgents, testCount) {
+      this.slack = new SlackMessage();
+      this.startTime = startTime;
+      this.testCount = testCount;
 
-            const startTimeFormatted = this.moment(this.startTime).format('M/D/YYYY h:mm:ss a');
+      const startTimeFormatted = this.moment(this.startTime).format('M/D/YYYY h:mm:ss a');
 
-            this.slack.sendMessage(`${emojis.rocket} ${'Starting TestCafe:'} ${bold(startTimeFormatted)}\n${emojis.computer} Running ${bold(testCount)} tests in: ${bold(userAgents)}\n`)
-        },
+      this.slack.sendMessage(`${emojis.rocket} ${'Starting TestCafe:'} ${bold(startTimeFormatted)}\n${emojis.computer} Running ${bold(testCount)} tests in: ${bold(userAgents)}\n`)
+    },
 
-        reportFixtureStart (name, path) {
-            this.currentFixtureName = name;
+    reportFixtureStart(name, path) {
+      this.currentFixtureName = name;
 
-            if (loggingLevel === loggingLevels.TEST) this.slack.addMessage(bold(this.currentFixtureName));
-        },
+      if (loggingLevel === loggingLevels.TEST) this.slack.addMessage(bold(this.currentFixtureName));
+    },
 
-        reportTestDone (name, testRunInfo) {
-            const hasErr = !!testRunInfo.errs.length;
-            let message = null;
+    reportTestDone(name, testRunInfo) {
+      const hasErr = !!testRunInfo.errs.length;
+      let message = null;
 
-            if (testRunInfo.skipped) {
-              message = `${emojis.fastForward} ${italics(name)} - ${bold('skipped')}`;
-            } else if (hasErr) {
-              message = `${emojis.fire} ${italics(name)} - ${bold('failed')}`;
-              this.renderErrors(testRunInfo.errs);
-            } else {
-              message = `${emojis.checkMark} ${italics(name)}`
-            }
+      if (testRunInfo.skipped) {
+        message = `${emojis.fastForward} ${italics(name)} - ${bold('skipped')}`;
+      } else if (hasErr) {
+        message = `${emojis.fire} ${italics(name)} - ${bold('failed')}`;
+        this.renderErrors(testRunInfo.errs);
+      } else {
+        message = `${emojis.checkMark} ${italics(name)}`
+      }
 
-            if (loggingLevel === loggingLevels.TEST) this.slack.addMessage(message);
-        },
+      if (loggingLevel === loggingLevels.TEST) this.slack.addMessage(message);
+    },
 
-        renderErrors(errors) {
-            errors.forEach((error, id) => {
-                this.slack.addErrorMessage(this.formatError(error, `${id + 1} `));
-            })
-        },
+    renderErrors(errors) {
+      errors.forEach((error, id) => {
+        this.slack.addErrorMessage(this.formatError(error, `${id + 1} `));
+      })
+    },
 
-        reportTaskDone (endTime, passed, warnings, result) {
-            const endTimeFormatted = this.moment(endTime).format('M/D/YYYY h:mm:ss a');
-            const durationMs  = endTime - this.startTime;
-            const durationFormatted = this.moment
-                .duration(durationMs)
-                .format('h[h] mm[m] ss[s]');
+    reportTaskDone(endTime, passed, warnings, result) {
+      const endTimeFormatted = this.moment(endTime).format('M/D/YYYY h:mm:ss a');
+      const durationMs = endTime - this.startTime;
+      const durationFormatted = this.moment
+        .duration(durationMs)
+        .format('h[h] mm[m] ss[s]');
 
-            const finishedStr = `${emojis.finishFlag} Testing finished at ${bold(endTimeFormatted)}\n`;
-            const durationStr = `${emojis.stopWatch} Duration: ${bold(durationFormatted)}\n`;
-            let summaryStr = '';
+      const finishedStr = `${emojis.finishFlag} Testing finished at ${bold(endTimeFormatted)}\n`;
+      const durationStr = `${emojis.stopWatch} Duration: ${bold(durationFormatted)}\n`;
+      let summaryStr = '';
 
-            if (result.skippedCount) summaryStr += `${emojis.fastForward} ${bold(`${result.skippedCount} skipped`)}\n`;
+      if (result.skippedCount) summaryStr += `${emojis.fastForward} ${bold(`${result.skippedCount} skipped`)}\n`;
 
-            if (result.failedCount) {
-              summaryStr += `${emojis.noEntry} ${bold(`${result.failedCount}/${this.testCount} failed`)}`
-            } else {
-              summaryStr += `${emojis.checkMark} ${bold(`${result.passedCount}/${this.testCount} passed`)}`
-            }
+      if (result.failedCount) {
+        summaryStr += `${emojis.noEntry} ${bold(`${result.failedCount}/${this.testCount} failed`)}`
+      } else {
+        summaryStr += `${emojis.checkMark} ${bold(`${result.passedCount}/${this.testCount} passed`)}`
+      }
 
-            const message = `\n\n${finishedStr} ${durationStr} ${summaryStr}`;
+      const message = `\n\n${finishedStr} ${durationStr} ${summaryStr}`;
 
-            this.slack.addMessage(message);
-            this.slack.sendTestReport(this.testCount - passed);
-        }
+      this.slack.addMessage(message);
+      this.slack.sendTestReport(this.testCount - passed);
     }
+  }
 }
--- test/utils/create-report.js
+++ test/utils/create-report.js
@@ -1,41 +1,41 @@
-var buildReporterPlugin = require('testcafe').embeddingUtils.buildReporterPlugin;
-var pluginFactory       = require('../../lib');
-var reporterTestCalls   = require('./reporter-test-calls');
+const buildReporterPlugin = require('testcafe').embeddingUtils.buildReporterPlugin;
+const pluginFactory = require('../../lib');
+const reporterTestCalls = require('./reporter-test-calls');
 
-module.exports = function createReport (withColors) {
-    var outStream = {
-        data: '',
+module.exports = function createReport(withColors) {
+  let outStream = {
+    data: '',
 
-        write: function (text) {
-            this.data += text;
-        }
-    };
-
-    var plugin = buildReporterPlugin(pluginFactory, outStream);
-
-    plugin.chalk.enabled = !plugin.noColors && withColors;
-    plugin.symbols       = { ok: '✓', err: '✖' };
-
-    // NOTE: disable errors coloring if we don't have custom
-    // error decorator. Default error colors may be prone to changing.
-    if (plugin.chalk.enabled && !pluginFactory().createErrorDecorator) {
-        var origFormatError = plugin.formatError;
-
-        plugin.formatError = function () {
-            plugin.chalk.enabled = false;
-
-            var result = origFormatError.apply(plugin, arguments);
-
-            plugin.chalk.enabled = true;
-
-            return result;
-        };
+    write: function (text) {
+      this.data += text;
     }
+  };
 
-    reporterTestCalls.forEach(function (call) {
-        plugin[call.method].apply(plugin, call.args);
-    });
+  let plugin = buildReporterPlugin(pluginFactory, outStream);
 
-    // NOTE: mock stack entries
-    return outStream.data.replace(/\s*?\(.+?:\d+:\d+\)/g, ' (some-file:1:1)');
+  plugin.chalk.enabled = !plugin.noColors && withColors;
+  plugin.symbols = {ok: '✓', err: '✖'};
+
+  // NOTE: disable errors coloring if we don't have custom
+  // error decorator. Default error colors may be prone to changing.
+  if (plugin.chalk.enabled && !pluginFactory().createErrorDecorator) {
+    let origFormatError = plugin.formatError;
+
+    plugin.formatError = function () {
+      plugin.chalk.enabled = false;
+
+      let result = origFormatError.apply(plugin, arguments);
+
+      plugin.chalk.enabled = true;
+
+      return result;
+    };
+  }
+
+  reporterTestCalls.forEach(function (call) {
+    plugin[call.method].apply(plugin, call.args);
+  });
+
+  // NOTE: mock stack entries
+  return outStream.data.replace(/\s*?\(.+?:\d+:\d+\)/g, ' (some-file:1:1)');
 };
